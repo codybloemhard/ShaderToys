dfdaeda07e84e615e9311bc7bc15bfce81e31eee
Cody Bloemhard <cblmhrd@live.nl>
2019-09-24 18:13:38 +0200

    directional light

--- wip.glsl
+++ wip.glsl
@@ -5,6 +5,9 @@
 
 float raymarch_d(vec3, vec3);
 float raymarch_s(vec3, vec3, float);
+
+vec3 ro;
+
 //https://stackoverflow.com/questions/45597118/fastest-way-to-do-min-max-based-on-specific-component-of-vectors-in-glsl
 vec2 minx(vec2 a, vec2 b)
 {
@@ -38,12 +41,12 @@ float capsule(vec3 p, float h, float r)
 
 float scene_dist(vec3 p){
     float t = iTime;
-    float z = 5.;
+    float z = 0.;
     float head = sphere(p,vec3(-1.,1,z), .8);
-    float eye0 = sphere(p,vec3(-1.,2.5,z+.3), .15);
-    float eye1 = sphere(p,vec3(-1.,2.5,z-.3), .15);
-    float stick0 = capsule(p-vec3(-1.,1.5,z+.3), 1., .05);
-    float stick1 = capsule(p-vec3(-1.,1.5,z-.3), 1., .05);
+    float balll = sphere(p,vec3(-1.,2.5,z+.3), .15);
+    float ballr = sphere(p,vec3(-1.,2.5,z-.3), .15);
+    float stickl = capsule(p-vec3(-1.,1.5,z+.3), 1., .05);
+    float stickr = capsule(p-vec3(-1.,1.5,z-.3), 1., .05);
     float c = .9;
     vec3 q = p;
     q.x = mod(max(0.,p.x),c*2.)-c;
@@ -56,17 +59,23 @@ float scene_dist(vec3 p){
     float legr = capsule(q-vec3(0,.1,z-.3), .5, .05);
     float footl = sphere(q,vec3(-.15,0,z+.3), .1);
     float footr = sphere(q,vec3(-.15,0,z-.3), .1);
+    float eyel = sphere(p,vec3(-1.65,1.4,z+.3), .1);
+    float eyer = sphere(p,vec3(-1.65,1.4,z-.3), .1);
+    float nose = sphere(p,vec3(-1.6,.9,z), .3);
     float floord = p.y;
-    float d = sdfBlendUnion(head, eye0, .1);
-    d = sdfBlendUnion(d, eye1, .1);
-    d = sdfBlendUnion(d, stick0, .15);
-    d = sdfBlendUnion(d, stick1, .15);
+    float d = sdfBlendUnion(head, balll, .1);
+    d = sdfBlendUnion(d, ballr, .1);
+    d = sdfBlendUnion(d, stickl, .15);
+    d = sdfBlendUnion(d, stickr, .15);
     d = sdfBlendUnion(d, body0, .1);
     d = sdfBlendUnion(d, body1, .15);
     d = sdfBlendUnion(d, legl, .15);
     d = sdfBlendUnion(d, legr, .15);
     d = sdfBlendUnion(d, footl, .3);
     d = sdfBlendUnion(d, footr, .3);
+    d = sdfBlendUnion(d, eyel, .1);
+    d = sdfBlendUnion(d, eyer, .1);
+    d = sdfBlendUnion(d, nose, .1);
     d = min(d, floord);
     return d;
 }
@@ -83,15 +92,19 @@ vec3 calc_normal(vec3 p) {
 
 vec3 calc_light(vec3 p){
 	vec3 n = calc_normal(p);
-    vec3 lpos = vec3(-10, 50, 20);
+    vec3 lpos = vec3(ro.x, 5., ro.z);
     vec3 tol = lpos - p;
     float dist = length(tol);
     vec3 ldir = tol / dist;
-    float str = max(AMBIENT,dot(n, ldir));
-    float e = str * 50. / dist;
-    if(e <= AMBIENT) return vec3(AMBIENT);
+    float str = max(0.,dot(n, ldir));
+    float e = str * 3. / dist;
     float shadow = raymarch_s(p + n * EPSILON * 2., ldir, 8.);
     e *= shadow;
+    //directional light (sun)
+    vec3 dld = normalize(vec3(0,10,0) - p);
+    float dl = max(0., dot(n,dld)) * 0.5;
+    dl *= raymarch_s(p + n * EPSILON * 2., dld, 8.);
+    e += dl;
     e = max(AMBIENT, e);
     return vec3(e);
 }
@@ -134,8 +147,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	
     vec3 col = vec3(0);
     
-    vec3 ro = vec3(-1. + sin(iTime*.2)*5., 1, 5. + cos(iTime*.2)*5.);
-    vec3 cd = normalize(vec3(-1, 1, 5) - ro);
+    ro = vec3(-1. + sin(iTime*.2)*5., 3, 0. + cos(iTime*.2)*5.);
+    vec3 cd = normalize(vec3(-1, 1, 0) - ro);
     vec3 ri = -normalize(cross(cd, vec3(0,1,0)));
     vec3 up = -normalize(cross(ri, cd));
     vec3 planep = ro + (cd*1.) + (ri*uv.x) + (up*uv.y);
