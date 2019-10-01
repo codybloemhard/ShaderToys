e2fed2dca9636fc63f28a4a089d43deef31b1b10
Cody Bloemhard <cblmhrd@live.nl>
2019-10-01 23:37:58 +0200

    change color framework

--- wip.glsl
+++ wip.glsl
@@ -3,13 +3,12 @@
 #define EPSILON 0.01
 #define AMBIENT 0.05
 
-vec2 raymarch_d(vec3, vec3);
+vec4 raymarch_d(vec3, vec3);
 float raymarch_s(vec3, vec3, float);
 
 vec3 ro;
 
 vec3 coloures[] = vec3[] ( //fool, orang is coloure!
-	vec3(.2,.6,.3),
 	vec3(.2,.2,.2),
 	vec3(.8,.6,.3),
 	vec3(.2,.2,.6),
@@ -18,12 +17,12 @@ vec3 coloures[] = vec3[] ( //fool, orang is coloure!
 	vec3(.2,.2,.2)
 );
 
-vec2 minx(vec2 a, vec2 b)
+vec4 minx(vec4 a, vec4 b)
 {
     float d = min(a.x, b.x);
-    float m = step(a.x, d+.0001) * a.y;
-    m += step(b.x, d+.0001) * b.y;
-    return vec2(d, m);
+    vec3 m = step(a.x, d+.0001) * a.yzw;
+    m += step(b.x, d+.0001) * b.yzw;
+    return vec4(d, m);
     //https://stackoverflow.com/questions/45597118/fastest-way-to-do-min-max-based-on-specific-component-of-vectors-in-glsl
     //return mix(a, b, clamp(step(b.x, a.x), 0., 1.));//artifact at horizon?
 }
@@ -58,7 +57,7 @@ float sdfCapsule(vec3 p, float h, float r)
     return length(p) - r;
 }
 
-vec2 caterpillar(vec3 p){
+vec4 caterpillar(vec3 p){
     float z = 0.;
     float head = sdfSphere(p,vec3(-1.,1,z), .8);
     float balll = sdfSphere(p,vec3(-1.,2.5,z+.3), .15);
@@ -84,8 +83,8 @@ vec2 caterpillar(vec3 p){
     d = sdfBlendUnionCol(d, vec2(ballr, 1.), .1);
     d = sdfBlendUnionCol(d, vec2(stickl, 2.), .15);
     d = sdfBlendUnionCol(d, vec2(stickr, 2.), .15);
-    d = sdfBlendUnionCol(d, vec2(body0, 3.), .1);
-    d = sdfBlendUnionCol(d, vec2(body1, 4.), .15);
+    d = sdfBlendUnionCol(d, vec2(body0, 4.), .1);
+    d = sdfBlendUnionCol(d, vec2(body1, 3.), .15);
     d = sdfBlendUnionCol(d, vec2(legl, 5.), .15);
     d = sdfBlendUnionCol(d, vec2(legr, 5.), .15);
     d = sdfBlendUnionCol(d, vec2(footl, 6.), .3);
@@ -93,19 +92,19 @@ vec2 caterpillar(vec3 p){
     d = sdfBlendUnionCol(d, vec2(eyel, 1.), .1);
     d = sdfBlendUnionCol(d, vec2(eyer, 1.), .1);
     d = sdfBlendUnionCol(d, vec2(nose, 1.), .1);
-    return d;
+    return vec4(d.x, vec3(1.));
 }
 
-vec2 scene(vec3 p){
-    vec2 boi = caterpillar(p);
+vec4 scene(vec3 p){
+    vec4 boi = caterpillar(p);
     /*because of the infite lenght of the body,
 	there is a weird shadow artifact in the neg x dir,
 	as if the body kinda is there(but it shouldnt).
 	so we cut that part off, but smoothly*/
     if(p.x < -3.)
         boi.x *= -p.x - 2.;
-    vec2 floord = vec2(p.y, 0.);
-    vec2 d = minx(boi, floord);
+    vec4 floord = vec4(p.y, vec3(0.));
+    vec4 d = minx(boi, floord);
     return d;
 }
 
@@ -146,14 +145,14 @@ vec3 calc_light(vec3 p){
     return e;
 }
 
-vec2 raymarch_d(vec3 ro, vec3 rd){
-	vec2 dO = vec2(0);
+vec4 raymarch_d(vec3 ro, vec3 rd){
+	vec4 dO = vec4(0);
     vec3 p;
     for(int i = 0; i < MAX_STEPS; i++){
     	p = ro + rd*dO.x;
-        vec2 dS = scene(p);
+        vec4 dS = scene(p);
         dO.x += dS.x;
-        dO.y = dS.y;
+        dO.yzw = dS.yzw;
         if(dO.x > MAX_DIST || dS.x < EPSILON)
             break;
     }
@@ -186,16 +185,16 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec3 up = -normalize(cross(ri, cd));
     vec3 planep = ro + (cd*1.) + (ri*uv.x) + (up*uv.y);
     vec3 rd = normalize(planep - ro);
-    vec2 res = raymarch_d(ro, rd);
+    vec4 res = raymarch_d(ro, rd);
     if(res.x > MAX_DIST){
         col = vec3(.3, .7, 1.);
         col = mix(col, vec3(.7, .7, .9), exp(rd.y*-10.));
     }else{
     	col = calc_light(ro + rd * res.x);
-       	float frac = fract(res.y);
-        int low = int(res.y);
-        int high = low + 1;
-        col *= mix(coloures[low], coloures[high], frac);
+       	if(res.yzw == vec3(0.))//terrain
+            col *= vec3(.2,.6,.3);
+       	else
+            col *= res.yzw;
     }
     col = pow(col, vec3(.4545));
     
